9fedf2eb109612d91b1776938ae2441d318bcd7b
Antonio-emprende <antonioramirezbogantes@gmail.com>
2024-10-29 20:13:57 -0600

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,104 @@
+// Archivo preguntas.js
+
+const preguntas = [
+    {
+        pregunta: "¿Qué es un seroma?",
+        opciones: ["A) Una acumulación de líquido seroso en los tejidos después de una cirugía", "B) Una infección bacteriana en los tejidos subcutáneos.", "C) Una reacción alérgica a los puntos de sutura", "D) Una cicatriz"],
+        respuestaCorrecta: "A"
+    },
+    {
+        pregunta: "¿Dónde suele acumularse el seroma abdominal?",
+        opciones: ["A) En los pulmones", "B) Dentro del estómago", "C) En el tejido celular subcutáneo o el espacio supraaponeurótico.", "D) Entre el músculo y el hueso"],
+        respuestaCorrecta: "C"
+    },
+    {
+        pregunta: "¿Cuál es una de las causas más comunes de seromas abdominales?",
+        opciones: ["A) Traumatismos leves", "B) Golpe", "C) Procedimientos que crean grandes espacios de disección, como una abdominoplastia", "D) Infecciones respiratorias"],
+        respuestaCorrecta: "C"
+    },
+    {
+        pregunta: "¿Qué componentes pueden estar presentes en el líquido seroso de un seroma?",
+        opciones: ["A) Grasas saturadas, glóbulos rojos y vitaminas", "B) Agua, iones, proteínas y colesterol", "C) Hormonas, ácidos grasos y enzimas digestivas", "D) Azúcares complejos y minerales"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Cuál es el tiempo promedio de aparición de los seromas tras una cirugía abdominal?",
+        opciones: ["A) 1-2 días después", "B) 3-5 días después", "C) Durante la primera o segunda semana postoperatoria.", "D) Después de un mes"],
+        respuestaCorrecta: "C"
+    },
+    {
+        pregunta: "¿Cuál es un síntoma común de un seroma?",
+        opciones: ["A) Secreción de líquido transparente o amarillo.", "B) Fiebre alta", "C) Náuseas", "D) Vómito"],
+        respuestaCorrecta: "A"
+    },
+    {
+        pregunta: "¿Cuál de los siguientes factores NO contribuye a la formación de seromas abdominales?",
+        opciones: ["A) Realizar actividad física controlada tras la cirugía", "B) Movimientos físicos intensos tras la cirugía", "C) Extensas áreas de separación de tejidos", "D)  Daño a los vasos linfáticos"],
+        respuestaCorrecta: "A"
+    },
+    {
+        pregunta: "¿Qué método NO se utiliza en el tratamiento de los seromas abdominales?",
+        opciones: ["A) Radioterapia", "B) Drenaje linfático", "C) Punciones con jeringa", "D) Uso de vendajes"],
+        respuestaCorrecta: "A"
+    },
+    {
+        pregunta: "**¿Cuál es una medida preventiva importante para reducir el riesgo de seroma?",
+        opciones: ["A) Mantener un estado de salud óptimo y seguir las instrucciones del médico", "B) Mantener una buena higiene", "C) Consumir solo líquidos durante una semana", "D) Usar vendajes de compresión antes de la cirugía"],
+        respuestaCorrecta: "A"
+    },
+    {
+        pregunta: "¿Cuál de las siguientes afirmaciones sobre el diagnóstico de seromas es correcta?",
+        opciones: ["A) Requiere una biopsia para confirmar la presencia de seroma", "B) Una evaluación visual y examen físico suelen ser suficientes para el diagnóstico", "C) Solo se diagnostican mediante estudios de laboratorio", "D) Se diagnostica solamente mediante rayos X "],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Qué tipo de cirugía puede aumentar el riesgo de formación de seromas abdominales?",
+        opciones: ["A) Cirugías de cataratas", "B) Cirugías estéticas como abdominoplastias o liposucción", "C) Cirguías de cataratas", "D) Cirugías ortopédicas"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Qué color suele tener el líquido seroso de un seroma no complicado?",
+        opciones: ["A) Verde", "B) Marrón", "C) Transparente o amarillo pálido", "D) Rojo intenso"],
+        respuestaCorrecta: "C"
+    },
+    {
+        pregunta: "¿Cuánto tiempo puede tardar en resolverse un seroma pequeño con tratamiento adecuado?",
+        opciones: ["A) 1 dia", "B) De 2 a 3 semanas", "C) Horas", "D) Nunca desaparece"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Cuál de las siguientes estructuras se encuentra superiormente en el abdomen y ayuda a contener los órganos abdominales?",
+        opciones: ["A) La pelvis", "B) El diafragma", "C) La columna vertebral", "D) Los pulmones"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Qué tipo de tejido contiene la fascia de Camper en la pared abdominal?",
+        opciones: ["A) Tejido muscular", "B) Tejido adiposo (grasa)", "C) Líquido cefalorraquideo", "D) Tejido óseo"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Qué espacio se encuentra por encima de la aponeurosis abdominal?",
+        opciones: ["A) Endoabdominal", "B) Supraaponeurótico", "C) Retroperitoneal", "D) Subcutáneo"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿El ejercicio intenso puede influir en un seroma?",
+        opciones: ["A) No", "B) A veces", "C) Sí", "D) Depende"],
+        respuestaCorrecta: "C"
+    },
+    {
+        pregunta: "¿Cuáles fascias componen el tejido subcutáneo de la pared anterolateral del abdomen?",
+        opciones: ["A) Camper y Scarpa", "B) Colágenas", "C) Tendinosas y elásticas", "D) Endoabdominal solamente"],
+        respuestaCorrecta: "A"
+    },
+    {
+        pregunta: "¿Un seroma siempre necesita cirugía?",
+        opciones: ["A) Sí", "B) Solamente en casos graves", "C) Siempre", "D) Nunca"],
+        respuestaCorrecta: "B"
+    },
+    {
+        pregunta: "¿Quiénes tienen un mayor riesgo de desarrollar un seroma abdominal después de una cirugía?",
+        opciones: ["A) Personas que se ejercitan diariamente", "B) Personas que han sufrido seromas con anterioridad", "C) Personas con buena salud", "D) Quienes nunca han tenido una cirugía"],
+        respuestaCorrecta: "B"
+    }
+];
